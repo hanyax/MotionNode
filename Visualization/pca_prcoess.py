0da5916b513ff95e34b27276008bf49d00b40188
kevinlin311tw <kevinlin311.tw@gmail.com>
2019-05-28 21:54:55 -0700

    extend pca codes for two placement

--- Visualization/pca_prcoess.py
+++ Visualization/pca_prcoess.py
@@ -3,7 +3,7 @@ import code
 from sklearn.decomposition import PCA
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import random
 
 
 def load_data(DATAPATH):
@@ -29,25 +29,50 @@ def load_data(DATAPATH):
 
 	return outdata
 
-def pca_process(data):
+
+def load_fack_data():
+	outdata = []
+	for i in range(1000):
+		x = random.random()
+		y = random.random()
+		z = random.random()
+		outdata.append([x,y,z])
+	return outdata
+
+def pca_process(data1, data2):
+	label1 = np.zeros(len(data1))
+	label2 = np.ones(len(data2))
+	# concatenate data
+
+	concatenated_data = np.concatenate((data1 ,data2))
+	concatenated_label = np.concatenate((label1 ,label2))
+
 	# training: learn a projection matrix
 	pca = PCA(n_components=2)
 
 	# apply the projection matrix, and get 2-dim data
-	newData = pca.fit_transform(data)
+	newData = pca.fit_transform(concatenated_data)
 
 	fig = plt.figure(figsize = (8,8))
 	plt.xlabel('Principal Component 1', fontsize = 15)
 	plt.ylabel('Principal Component 2', fontsize = 15)
-	plt.scatter(newData[:,0],newData[:,1])
+
+	for i in range(len(label1)):
+		plt.scatter(newData[i,0],newData[i,1],c='red')
+
+	for i in range(len(label1),len(label2)):
+		plt.scatter(newData[i,0],newData[i,1],c='blue')
+
 	plt.grid()
 	plt.show()
+
 
 
 def main():
 	DATAPATH = 'data.txt'
 	data = load_data(DATAPATH)
-	pca_process(data)
+	data2 = load_fack_data()
+	pca_process(data, data2)
 
 if __name__ == '__main__':
     main()
